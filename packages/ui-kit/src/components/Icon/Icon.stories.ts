b05514dbc5673beb7501dfac63c806d9d726f703
Christopher Kiessling <Crsk@mail.com>
2023-06-05 05:56:59 -0400

    refactor(ui-kit): support theme for icons

--- packages/ui-kit/src/components/Icon/Icon.stories.ts
+++ packages/ui-kit/src/components/Icon/Icon.stories.ts
@@ -11,26 +11,60 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-export const ArrowRight: Story = {
+export const ArrowRightDark: Story = {
   args: {
     type: 'arrow-right',
+    theme: 'dark',
   },
 }
 
-export const Sort: Story = {
+export const ArrowRightLight: Story = {
+  args: {
+    type: 'arrow-right',
+    theme: 'light',
+  },
+}
+
+export const SortDark: Story = {
   args: {
     type: 'sort',
+    theme: 'dark',
   },
 }
 
-export const SortAsc: Story = {
+export const SortLight: Story = {
+  args: {
+    type: 'sort',
+    theme: 'light',
+  },
+}
+
+export const SortAscDark: Story = {
   args: {
     type: 'sort-asc',
+    theme: 'dark',
   },
 }
 
-export const SortDesc: Story = {
+export const SortAscLight: Story = {
+  args: {
+    type: 'sort-asc',
+    theme: 'light',
+  },
+}
+
+export const SortDescDark: Story = {
   args: {
     type: 'sort-desc',
+    theme: 'dark',
+  },
+}
+
+export const SortDescLight: Story = {
+  args: {
+    type: 'sort-desc',
+    theme: 'light',
+  },
+}
   },
 }
--- packages/ui-kit/src/components/Icon/Icon.tsx
+++ packages/ui-kit/src/components/Icon/Icon.tsx
@@ -1,34 +1,41 @@
 import React from 'react'
 import './icon.scss'
-import { ArrowRight, Sort, SortAsc, SortDesc } from './vectors'
+import { ArrowRight, Search, Sort, SortAsc, SortDesc } from './vectors'
 
 interface IconProps {
-  type: 'arrow-right' | 'sort' | 'sort-desc' | 'sort-asc'
+  type: 'arrow-right' | 'sort' | 'sort-desc' | 'sort-asc' | 'search'
+  theme?: 'light' | 'dark'
 }
 
-const Icon = ({ type = 'arrow-right' }: IconProps) => {
+const Icon = ({ type = 'arrow-right', theme = 'dark' }: IconProps) => {
   if (type === 'arrow-right')
     return (
       <div data-testid="arrow-right" className="icon-container">
-        {ArrowRight}
+        {ArrowRight(theme)}
       </div>
     )
   if (type === 'sort')
     return (
       <div data-testid="sort" className="icon-container">
-        {Sort}
+        {Sort(theme)}
       </div>
     )
   if (type === 'sort-desc')
     return (
       <div data-testid="sort-desc" className="icon-container">
-        {SortDesc}
+        {SortDesc(theme)}
       </div>
     )
   if (type === 'sort-asc')
     return (
       <div data-testid="sort-asc" className="icon-container">
-        {SortAsc}
+        {SortAsc(theme)}
+      </div>
+    )
+  if (type === 'search')
+    return (
+      <div data-testid="search" className="icon-container">
+        {Search(theme)}
       </div>
     )
 
