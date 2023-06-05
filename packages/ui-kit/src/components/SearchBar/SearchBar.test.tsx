529617b314b6642413c47b74b4c423893aadbc56
Christopher Kiessling <Crsk@mail.com>
2023-06-05 15:22:13 -0400

    chore(iu-kit): remove unused import

--- packages/ui-kit/src/components/SearchBar/SearchBar.test.tsx
+++ packages/ui-kit/src/components/SearchBar/SearchBar.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, prettyDOM, render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { SearchBar } from './SearchBar'
 
 vi.mock('../Icon/Icon', () => ({
