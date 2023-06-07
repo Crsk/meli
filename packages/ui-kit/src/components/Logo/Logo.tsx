bc1e41e12f5acc08a83c77b84bcc4b0e05b7a20b
Christopher Kiessling <Crsk@mail.com>
2023-06-07 17:01:14 -0400

    style(ui-kit): sort import

--- packages/ui-kit/src/components/Logo/Logo.tsx
+++ packages/ui-kit/src/components/Logo/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
+import { Theme } from 'shared/src/types'
 import { LogoVector } from './Logo.vector'
 import { LogoSize } from './properties'
-import { Theme } from 'shared/src/types'
 
 interface LogoProps {
   theme?: Theme
