c54b96ba44bc064dc6fd637ebd5110a2df491049
Christopher Kiessling <Crsk@mail.com>
2023-06-11 20:26:15 -0400

    fix(ui-kit): handle item card long title

--- packages/ui-kit/src/components/ProductDetailsCard/ProductDetailsCard.stories.tsx
+++ packages/ui-kit/src/components/ProductDetailsCard/ProductDetailsCard.stories.tsx
@@ -65,3 +65,32 @@ export const ProductDetailsCardLight: Story = {
     },
   },
 }
+
+export const ProductDetailsCardLongTitle: Story = {
+  args: {
+    theme: 'light',
+    productDetails: {
+      id: 'MLA123456789',
+      title:
+        'Apple iPhone 11 (64 GB) - Negro | Apple iPhone 11 (64 GB) - Negro | Apple iPhone 11 (64 GB) - Negro | Apple iPhone 11 (64 GB) - Negro',
+      price: {
+        currency: 'CLP',
+        amount: 399,
+        decimals: 990,
+      },
+      originalPrice: {
+        amount: 669,
+        decimals: 990,
+      },
+      picture: null,
+      condition: '12x $33.332 sin interés',
+      freeShipping: true,
+      seller: 'Apple',
+      reviewCount: 1089,
+      colorCount: 5,
+      soldQuantity: 200,
+      description:
+        'Lorem ipsum dolor sit amet. Eum nisi similique et consequatur natus in quod maiores aut voluptatem voluptatem et inventore repellendus et aperiam provident ex officia vitae. Aut dolor officiis aut eveniet dolores et mollitia vero eos aperiam voluptas eum nesciunt veniam qui quis harum aut necessitatibus expedita. Qui maiores voluptatem in doloribus maiores ad praesentium reprehenderit. Aut numquam quae eum internos quaerat hic voluptatem beatae non sunt deserunt aut delectus dolorum aut minus architecto. Nam error veritatis ut excepturi odio sed rerum galisum aut dolor libero. Est explicabo deleniti sit tempore provident in repellendus reprehenderit. Cum iusto nostrum sed illo vero non harum ullam hic culpa quas ut ducimus optio qui facilis tenetur. Est laudantium blanditiis est reprehenderit iste in beatae quaerat et rerum ipsam. Ut minima incidunt est molestiae recusandae cum itaque vitae non autem reiciendis At ipsum accusantium vel dicta eius aut numquam reprehenderit! Vel nulla voluptatem aut ipsa repellendus et quae distinctio est quam iste ut totam dolore aut culpa ratione est itaque quia.',
+    },
+  },
+}
